9fed8a7cbedb845980aeca748e609d7ada67ba26
刘学 <liuxue@infohold.com.cn>
2014-10-26 14:53:50 +0800

    修改数据结构

--- gateway/models.py
+++ gateway/models.py
@@ -7,26 +7,24 @@ Created on Oct 6, 2014
 from mongoengine import Document, StringField, IntField, DateTimeField, \
 DictField, ReferenceField
 import datetime
-class Base(Document):
+
+class Server(Document):
+    name = StringField(max_length=40, unique=True, required=True)
+    desc = StringField(max_length=40, required=True)
+    ip = StringField(max_length=40, required=True)
+    port = IntField(max_value=9999, min_value=1000, required=True)
     created_at = DateTimeField(required=True)
     update_at = DateTimeField(required=True)
     
-    meta = {'allow_inheritance': True}
     
     def save(self, *args, **kwargs):
         if not self.created_at:
             self.created_at = datetime.datetime.now()
         self.update_at = datetime.datetime.now()
-        return super(Base, self).save(*args, **kwargs)
-
-class Server(Base):
-    name = StringField(max_length=40, unique=True, required=True)
-    desc = StringField(max_length=40, required=True)
-    ip = StringField(max_length=40, required=True)
-    port = IntField(max_value=9999, min_value=1000, required=True)
+        return super(Server, self).save(*args, **kwargs)
 
     
-class Method(Base):
+class Method(Document):
     '''
     {
         'name':name,
@@ -43,8 +41,16 @@ class Method(Base):
     desc = StringField(max_length=40)
     server = ReferenceField(Server)
     url = DictField()
-
-class Appkey(Base):
+    created_at = DateTimeField(required=True)
+    update_at = DateTimeField(required=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.created_at:
+            self.created_at = datetime.datetime.now()
+        self.update_at = datetime.datetime.now()
+        return super(Method, self).save(*args, **kwargs)
+    
+class Appkey(Document):
     '''
     {
         'name':name,
@@ -58,3 +64,12 @@ class Appkey(Base):
     secert_key = StringField(max_length=40)
     desc = StringField(max_length=40)
     access = DictField(default={})
+    created_at = DateTimeField(required=True)
+    update_at = DateTimeField(required=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.created_at:
+            self.created_at = datetime.datetime.now()
+        self.update_at = datetime.datetime.now()
+        return super(Appkey, self).save(*args, **kwargs)
+    
